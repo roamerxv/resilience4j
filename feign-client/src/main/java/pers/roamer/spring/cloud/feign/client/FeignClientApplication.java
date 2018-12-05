e67be6bcf9089b01423a477377b954eba17e923b
徐泽宇 <xu.zeyu@z2a.com.cn>
2018-12-05 14:06:51 +0800

    fix errors

--- feign-client/src/main/java/pers/roamer/spring/cloud/feign/client/FeignClientApplication.java
+++ feign-client/src/main/java/pers/roamer/spring/cloud/feign/client/FeignClientApplication.java
@@ -10,13 +10,9 @@ package pers.roamer.spring.cloud.feign.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableEurekaClient
 @EnableFeignClients
 public class FeignClientApplication {
--- feign-client/src/main/java/pers/roamer/spring/cloud/feign/client/service/IEurekaClientFeign.java
+++ feign-client/src/main/java/pers/roamer/spring/cloud/feign/client/service/IEurekaClientFeign.java
@@ -4,12 +4,11 @@ package pers.roamer.spring.cloud.feign.client.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pers.roamer.spring.cloud.feign.client.config.FeignConfig;
 
 /**
  * @author roamer
  */
-@FeignClient(value = "service-hi" , configuration = FeignConfig.class , fallback = HiServiceHystrixRollback.class)
+@FeignClient(value = "service-hi" )
 public interface IEurekaClientFeign {
 
     @GetMapping("/hi")
