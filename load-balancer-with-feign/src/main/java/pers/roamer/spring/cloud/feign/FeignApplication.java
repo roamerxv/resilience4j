dad2809f41ccddc855852764619ef124ba211ff5
徐泽宇 <xu.zeyu@z2a.com.cn>
2018-12-18 18:22:40 +0800

    调整  spring boot cloud version

--- load-balancer-with-feign/src/main/java/pers/roamer/spring/cloud/feign/FeignApplication.java
+++ load-balancer-with-feign/src/main/java/pers/roamer/spring/cloud/feign/FeignApplication.java
@@ -2,9 +2,7 @@ package pers.roamer.spring.cloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @program: resilience4j
@@ -14,9 +12,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  **/
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
-@EnableRedisHttpSession
 public class FeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignApplication.class, args);
